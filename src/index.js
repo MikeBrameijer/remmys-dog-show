3ab018d83cb1a82ec2fbc852418f9c9868de7742
Mike Brameijer <mike.brameijer@dynavistics.com>
2019-11-22 13:21:56 -0500

    Dog API fetch and render img

--- src/index.js
+++ src/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function Square(props) {
+//     return (
+//         <button
+//             className="square"
+//             onClick={props.onClick}
+//         >
+//             {props.value}
+//         </button>
+//     );
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// class Board extends React.Component {
+
+
+
+
+//     renderSquare(i) {
+//         return (
+//             <Square
+//                 value={this.props.squares[i]}
+//                 onClick={() => this.props.onClick(i)}
+//             />
+//         );
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 <div className="board-row">
+//                     {this.renderSquare(0)}
+//                     {this.renderSquare(1)}
+//                     {this.renderSquare(2)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(3)}
+//                     {this.renderSquare(4)}
+//                     {this.renderSquare(5)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(6)}
+//                     {this.renderSquare(7)}
+//                     {this.renderSquare(8)}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dogImg: String,
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            this.setState({dogImg: data.message});
+            console.log(this.state.dogImg);
+        });
+        
+    }
+
+    render() {
+        let randomDogImg = this.state.dogImg;
+
+        return (
+            <div className="main">
+                <h1>Remmy's Dog Show</h1>
+                <img src={randomDogImg}></img>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('root')
+);
+
