4e881155761490d0f5a869bc337f90e03b1860c4
Mike Brameijer <mike.brameijer@dynavistics.com>
2019-11-22 16:44:35 -0500

    added random captions passed through the state. moved caption setting to condition statement.

--- src/index.js
+++ src/index.js
@@ -6,32 +6,44 @@ class MainApp extends React.Component {
     constructor() {
         super();
         this.state = {
-            dogImg: []
+            dogImg: [],
+            randomCaption: []
         };
+    // this might be from working in TypeScript too long, but I would like to know why I cant just declare dogImg: String.
+    // I had originally declared it as dogImg: String and work fine but it wouldn't let me do use my ternary operator for the intro picture and text
+    // with this statement (!this.state.dogImg ? blah : blah). instead I have to set state.dogImg as an array and use this logic (this.state.dogImg.length === 0)
+    // to get my desired results.
+        
     }
 
     // the following is a API call for fetching the dog img
     nextDog() {
+        let randomSayingArray = ["Look at this pup!", "Wow!", "This pup is awesome at fetch!", "Who's a good pup?!"]
         fetch('https://dog.ceo/api/breeds/image/random')
         .then(results => {
             return results.json();
         }).then(data => {
-            this.setState({dogImg: data.message});
-            console.log(this.state.dogImg);
+            this.setState({dogImg: data.message,
+                randomCaption: randomSayingArray[Math.floor(Math.random() *4)]
+            });
         });
         
     }
     
 
     render() {
-        console.log(this.state.dogImg );
         let mainImg;
         let introRemmy = "./remmy-relax.jpg";
+        let caption;
 
+        // This condition decides whether to display the intro img and caption
+        // I know the this.state.dogImg.length === 0 is cheesy I would wouldn't mind talking about it
         if(this.state.dogImg.length === 0){
             mainImg = introRemmy;
+            caption = `Hi I'm Remmy, I'm Mike's best friend! I love other dogs, would you like to see some of my favorite pals!`;
         }else{
             mainImg = this.state.dogImg;
+            caption = this.state.randomCaption;
         }
 
         return (
@@ -44,10 +56,9 @@ class MainApp extends React.Component {
                 <div class="main">
                 
                     <img class="dog-display" src={mainImg}></img>
-                    {/* when the pages first loads a picture of Remmy with introduce the users to the dog tour */}
-                    <p>{this.state.dogImg.length === 0 ? `Hi my name is Remmy! I love other dogs, if you click the button below I'll take you on a tour of some of my favorite Pals!` : 'Look at this Pup!'}</p>
-                    {/* the buttons text will change after the user gets started */}
+                    <p>{caption}</p>
                     <button class="next-dog-btn" onClick={() => this.nextDog()}>
+                        {/* this should really be moved to the condition statement above but i wanted to try out how to do and ngIf in React */}
                         {this.state.dogImg.length === 0 ? `Click to get started!` : `Click to see the next pup`}
                     </button>
                 </div>
